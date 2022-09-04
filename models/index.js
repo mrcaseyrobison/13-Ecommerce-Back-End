17135dc29bfeacb7ed57de43a5152816f7a25548
mrcaseyrobison <mrcaseyrobison@gmail.com>
2022-09-03 19:48:35 -0700

    index.js complete

--- models/index.js
+++ models/index.js
@@ -5,12 +5,26 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: "category_id",
+});
 
 // Categories have many Products
+Category.hasMany(Product, {
+  foriegnKey: "category_id",
+});
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  foreignKey: "product_id",
+});
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongToMany(Product, {
+  through: ProductTag,
+  foriengKey: "tag_id",
+});
 
 module.exports = {
   Product,
