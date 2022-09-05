b8efd601f4abd653597d4f3cb405a83795d80cb4
mrcaseyrobison <mrcaseyrobison@gmail.com>
2022-09-05 13:44:44 -0700

    delete route added

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -62,8 +62,23 @@ router.put('/:id', async(req, res) => {
   }
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async(req, res) => {
   // delete on tag by its `id` value
+  try {
+    const deletedTag = await Tag.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+    if (!deletedTag) {
+      res.status(404).json({ message: "Nothing found with that ID"});
+      return;
+    }
+
+    res.status(200).json(deletedTag);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
