f2e6c1582e0b19b6ca3bf9a1accfdc710612b7f9
mrcaseyrobison <mrcaseyrobison@gmail.com>
2022-09-05 14:10:30 -0700

    all todos complete but still error

--- models/index.js
+++ models/index.js
@@ -21,7 +21,7 @@ Product.belongsToMany(Tag, {
 });
 
 // Tags belongToMany Products (through ProductTag)
-Tag.belongToMany(Product, {
+Tag.belongsToMany(Product, {
   through: ProductTag,
   foriengKey: "tag_id",
 });
